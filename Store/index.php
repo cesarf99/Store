ea91f2fe652213c58b7a6eb6fdfa9b67d9758458
cesarf99 <64504389+cesarf99@users.noreply.github.com>
2021-06-03 01:12:23 +0100

    Add files via upload

---
+++ Store/index.php
@@ -0,0 +1,43 @@
+<?php
+include ('Config\Database.php');
+
+$b= new Database();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Store</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    </head>
+
+    <body>
+
+        <!-- Header Section -->
+        <header>
+            <div class="head1">Money4Clothes Store</div>
+            <div class="head2">Low Prices High Quality Clothes</div>
+        </header>
+
+        <!-- Menu Navigation Bar -->
+        <div class="menu">
+            <a href="#home">HOME</a>
+            <a href="#news">NEWS</a>
+            <a href="#notification">NOTIFICATIONS</a>
+            <div class="menu-log">
+                <a href="#login">LOGIN</a>
+            </div>
+        </div>
+
+        <!-- Body section -->
+        <div class = "body_sec">
+            <section id="Content">
+                <h3>Content section</h3>
+            </section>
+        </div>
+
+        <!-- Footer Section -->
+        <footer>Clothing Store , Vila Real 2021</footer>
+    </body>
+</html>
