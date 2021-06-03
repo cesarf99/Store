b8dbd17044dfd7b31839556089c0f0eb6ccf6007
cesar <cesarfreitasutad@gmail.com>
2021-06-03 02:04:54 +0100

    First

---
+++ Models/User.php
@@ -0,0 +1,48 @@
+<?php
+
+class User {
+    private $id;
+    private $name;
+    private $username;
+    private $password;
+    private $address;
+    
+     public function __construct() {
+        $this->db = new Database;
+    }
+}  
+
+
+//<?php
+//
+//require ('Config\Db.php');
+//
+//class usermodel {
+//    
+//}
+//
+//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//    $conn = connect();
+//
+//    $sql = "INSERT INTO user (`USERID`, `NAME`, `USERNAME`, `PASSWORD`, `ADDRESS`, `EMAIL`) VALUES (NULL, 'César Freitas', 'cesar', '123456', 'rua rua', 'cesar@email.com')";
+//    $result = mysqli_query($conn, $sql);
+//
+//    $user_id = mysqli_insert_id($conn);
+//
+//    $sql1 = "INSERT INTO roles (`ROLEID`, `ROLENAME`, `USERID`) VALUES (NULL, 'Admin', '$user_id')";
+//    $result1 = mysqli_query($conn, $sql1);
+//
+//    $q = mysqli_stmt_init($conn);
+//    if ($result == 1 || $result1 == 1) {
+//        echo 'Inserted with success';
+//    } else {
+//        echo 'failed';
+//    }
+//
+//    $conn->close();
+//} 
+
+
+
+
+
